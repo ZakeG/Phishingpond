aef6c62c1c0b092327bf9e9dbf0af8c805133bc8
Couchpirate <chuu@live.se>
2018-09-25 00:33:02 +0200

    Made companies

--- Phishing Pond/PhishingPond.js
+++ Phishing Pond/PhishingPond.js
@@ -2,42 +2,62 @@ $(document).ready(function() {
 
 	var activeCell = false;
 	var targetedCell;
+	var cellDesc;
+	var info;
 
-	$("body").on("click", "body", function() {
-		if(activeCell == true){
-			document.getElementById("myDropdown").classList.toggle("show");
-			//activeCell = false;
-		}
-	});
+	var cellInfo = [["Company 1", "a small company", "training 1 <br> training 2"],
+					["Company 2", "a medium company", "training 3 <br> training 4"],
+					["Company 3", "a big company", "training 5 <br> training 6"]];
 
-	$("td").each(function() {
+
+	$("#table td").click(function() {     
+ 
+        var column_num = parseInt( $(this).index());
+	    var row_num = parseInt( $(this).parent().index());
+        targetedCell = $(this).attr('id');
+        info = $(this)[ 0 ];
+        $('#title').html(jQuery.data(info, "test").Title);
+        $('#desc').html(jQuery.data(info,"test").Description);
+        $('#secText').html(jQuery.data(info,"test").SecInfo);
+       	//alert( "Row_num =" + row_num + "  ,  Rolumn_num ="+ column_num );   
+    });
+
+    $('#table td').mouseleave(function(){
+    	if(activeCell == true)
+    	document.getElementById("myDropdown").classList.toggle("show");
+    	activecell = false;
+    });
+
+	$("#table td").each(function() {
 		$(this).html($(this).attr('id'))//$(this).attr('id').toString();
+        info = $(this)[ 0 ];
+        jQuery.data( info, "test", {
+			Title: cellInfo[parseInt($(this).index())][0],
+			Description: cellInfo[parseInt($(this).index())][1],
+			SecInfo: cellInfo[parseInt($(this).index())][2]
+		}); 
   	});
 
 	$("#table").on("click", "td", function() {
 		if(activeCell == false){
      	document.getElementById("myDropdown").classList.toggle("show");
      	activeCell = true;
-    } else {
+    }else {
     	document.getElementById("myDropdown").classList.toggle("show");
      	activeCell = false;
     }
 
-	$("#link1").on("click", function(){ 
-		alert("wan");
+	$("#link1").on("click", function(){
 
 	})
 	$("#link2").on("click", function(){ 
-		alert("tu");
+		alert(jQuery.data(info, "test" ).Description);
 
 	})
 	$("#link3").on("click", function(){ 
-		alert("tree");
+		alert(jQuery.data(info, "test" ).SecInfo);
 
 	})
-
-
-
      	 
    });
 		$("body").mousemove(function( event ) {
