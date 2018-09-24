e7ecf01238ce5db505f1faa9ccc250d3b5d70e3f
Couchpirate <chuu@live.se>
2018-09-25 00:47:35 +0200

    idktbh

--- Phishing Pond/PhishingPond.js
+++ Phishing Pond/PhishingPond.js
@@ -7,7 +7,12 @@ $(document).ready(function() {
 
 	var cellInfo = [["Company 1", "a small company", "training 1 <br> training 2"],
 					["Company 2", "a medium company", "training 3 <br> training 4"],
-					["Company 3", "a big company", "training 5 <br> training 6"]];
+					["Company 3", "a big company", "training 5 <br> training 6"],
+					["Company 4", "an even bigger company", "training 7 <br> training 8"],
+					["Company 5", "a massive company", "training 9 <br> training 10"],
+					["Company 6", "a humongous company", "training 11 <br> training 12"],
+					["Company 7", "an absolutely ridiculously big company", "training 13 <br> training 14"],
+					["Company 8", "a company", "training 15 <br> training 16"]];
 
 
 	$("#table td").click(function() {     
@@ -22,11 +27,13 @@ $(document).ready(function() {
        	//alert( "Row_num =" + row_num + "  ,  Rolumn_num ="+ column_num );   
     });
 
+	/*
     $('#table td').mouseleave(function(){
     	if(activeCell == true)
     	document.getElementById("myDropdown").classList.toggle("show");
     	activecell = false;
     });
+    */
 
 	$("#table td").each(function() {
 		$(this).html($(this).attr('id'))//$(this).attr('id').toString();
@@ -48,14 +55,25 @@ $(document).ready(function() {
     }
 
 	$("#link1").on("click", function(){
-
+		if(jQuery.data(info,"test").secInfo.search("training 1") != -1)
+			alert("resisted");
+		else{
+			alert("infected");
+		}
 	})
 	$("#link2").on("click", function(){ 
-		alert(jQuery.data(info, "test" ).Description);
-
+		if(jQuery.data(info,"test").secInfo.search("training 2") != -1)
+			alert("resisted")
+		else{
+			alert("infected")
+		}
 	})
 	$("#link3").on("click", function(){ 
-		alert(jQuery.data(info, "test" ).SecInfo);
+		if(jQuery.data(info,"test").secInfo.search("training 3") != -1)
+			alert("resisted")
+		else{
+			alert("infected")
+		}
 
 	})
      	 
