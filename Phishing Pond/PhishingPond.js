9222e6019bb82b0e8f76e3f3ea8b12ca99fe9316
Couchpirate <chuu@live.se>
2018-09-28 11:32:50 +0200

    MADE BIG IMPROVEMENT

--- Phishing Pond/PhishingPond.js
+++ Phishing Pond/PhishingPond.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-
-	var top = $("buttonHolder").css('margin-top');
-
+	var targetedCell;
+	var cellDesc;
+	var info;
+	var curID;
+	var activeState = false;
+	var tempNum = 0;
+	var infectedLevel = 0;
+	var cellInfo = [	/*Name*/ /*Description*/ /*Security options*/ /*Answer options*/ 
+					[["Company 1"],["a small company"],[1],["hello", "it", "is", "me"]],
+					[["Company 2"], ["a medium company"], [1, 3],["hello", "it", "is", "me"]],
+					[["Company 3"], ["a big company"], [2],["hello", "it", "is", "me"]],
+					[["Company 4"], ["an even bigger company"], [2, 3],["hello", "it", "is", "me"]],
+					[["Company 5"], ["a massive company"], [1, 2, 3],["hello", "it", "is", "me"]],
+					[["Company 6"], ["a humongous company"], [1, 3],["hello", "it", "is", "me"]],
+					[["Company 7"], ["an absolutely ridiculously big company"], [2, 3, 4],["hello", "it", "is", "me"]],
+					[["Company 8"], ["a company"], [1, 2, 4],["hello", "it", "is", "me"]]
+					];	
+	var SecurityLevels = ["Spam mail filter", "Malware detection", "Spoofed link training", "Anti-spear phishing training"];
+	var cellTest = [["c00"], ["c10"], ["c11"], ["c21", "c12"],["c13"]];	
+	var currentCell = 0;
+   	var correctAnswer;
+   	var clicked;
+	
 	function TransitionOptions() {
 		if($("#buttonHolder").css("margin-top") == "0px"){
 			$("#buttonHolder").css("margin-top", "-125px");
@@ -12,82 +32,48 @@ $(document).ready(function() {
 			$("#buttonHolder td").css("color", "black");
 		}
 	}
-   	//document.getElementById("buttonHolder").classList.toggle("show");
-   	function hideyhide(){
-   		$("#c03").css("visibility", "hidden");	
-   	}
-   	function showyshow(){
-   		$("#c03").css("visibility", "visible");
-   	}
-
-
-	var targetedCell;
-	var cellDesc;
-	var info;
-	var curID;
-	var activeState = false;
-
-	var infectedLevel = 0;
-
-	var cellInfo = [	/*Name*/ /*Description*/ /*Security options*/ /*Answer options*/ 
-					[["Company 1"],["a small company"],[1, 2],["hello", "it", "is", "me"]],
-					[["Company 2"], ["a medium company"], [1, 2],["hello", "it", "is", "me"]],
-					[["Company 3"], ["a big company"], [1, 2],["hello", "it", "is", "me"]],
-					[["Company 4"], ["an even bigger company"], [1, 2],["hello", "it", "is", "me"]],
-					[["Company 5"], ["a massive company"], [1, 2],["hello", "it", "is", "me"]],
-					[["Company 6"], ["a humongous company"], [1, 2],["hello", "it", "is", "me"]],
-					[["Company 7"], ["an absolutely ridiculously big company"], [1, 2],["hello", "it", "is", "me"]],
-					[["Company 8"], ["a company"], [1, 2],["hello", "it", "is", "me"]]
-					];	
-	var SecurityLevels = [""]
-
-	var cellTest = [["c00"], ["c10"], ["c11"], ["c21", "c12"],["c13", "c03", "c31"]];	
-
-
-/*
-	$("#table td").click(function() {     
-
-       	//alert( "Row_num =" + row_num + "  ,  Rolumn_num ="+ column_num );   
-    });
-    */
-	/*	
-    $('#table td').mouseleave(function(){
-    	if(activeCell == true){
-    		document.getElementById("myDropdown").classList.toggle("show");
-    		activeCell = false;
-    	}
-    });
-	*/
 
 	function assignData(){
 		$("#table td").each(function() {
 			var tempString = "";
-			if($(this).attr('id') != "c00") $(this).css('visibility', 'hidden');
-			$(this).html($(this).attr('id'))//$(this).attr('id').toString();
-			for(i = 0; i < cellInfo[parseInt($(this).index())][2].length; i++)				
-				tempString += cellInfo[parseInt($(this).index())][2][i] + "<br>";
-        	info = $(this)[ 0 ];
-        	jQuery.data( info, "test", {
-			Title: cellInfo[parseInt($(this).index())][0][0],
-			Description: cellInfo[parseInt($(this).index())][1][0],
-			SecInfo: tempString,
-			Option1: cellInfo[parseInt($(this).index())][3][0],
-			Option2: cellInfo[parseInt($(this).index())][3][1],
-			Option3: cellInfo[parseInt($(this).index())][3][2],
-			Option4: cellInfo[parseInt($(this).index())][3][3],
-			//Answer: cellInfo[parseInt($(this).index())][4][0]
-			}); 
-        });
+			for(i = 0; i < cellTest.length; i++){
+				for(j = 0; j < cellTest[i].length; j++){
+					if($(this).attr('id') == cellTest[i][j]){
+						console.log($(this).attr('id') + " = true");
+						$(this).html($(this).attr('id'));
+						for(k = 0; k < cellInfo[tempNum][2].length; k++)				
+							tempString += SecurityLevels[k] + "<br>";
+						info = $(this)[ 0 ];
+			        	jQuery.data( info, "test", {
+			        	CompanyNumber: tempNum,
+						Title: cellInfo[tempNum][0][0],
+						Description: cellInfo[tempNum][1][0],
+						SecInfo: tempString,
+						Option1: cellInfo[tempNum][3][0],
+						Option2: cellInfo[tempNum][3][1],
+						Option3: cellInfo[tempNum][3][2],
+						Option4: cellInfo[tempNum][3][3],
+						});
+						tempNum++;
+					}
+					else if($(this).attr('id') != "c00"){
+						$(this).css("visibility", "hidden");
+					}
+				}
+			}
+		});
 	}
 
 	assignData();
 
 	function checkGrid(){
 		$("#table td").each(function() {
+			if(infectedLevel < cellTest.length){
 			for(i = 0; i < cellTest[infectedLevel].length; i++){
 				if($(this).attr('id') == cellTest[infectedLevel][i]) {
 	        	$(this).css("visibility", "visible");
 				}
+			}
 	        }
 	  	});
 	}
@@ -98,7 +84,9 @@ $(document).ready(function() {
    	}
 
 	$("#table").on("click", "td", function() {
+		shuffle($("#buttons"));
         info = $(this)[ 0 ];
+        correctAnswer = cellInfo[jQuery.data(info, "test").CompanyNumber][2];
         $('#title').html(jQuery.data(info, "test").Title);
         $('#desc').html(jQuery.data(info,"test").Description);
         $('#secText').html(jQuery.data(info,"test").SecInfo);
@@ -108,39 +96,60 @@ $(document).ready(function() {
         $('#link4').html(jQuery.data(info,"test").Option4);
         if(activeState == false){
         	TransitionOptions();
-    		//document.getElementById("buttonHolder").classList.toggle("show");
     		activeState = true;
     	}
 		else if((targetedCell == $(this).attr('id') || targetedCell == undefined) && activeState == true){
-			TransitionOptions()
-     		//document.getElementById("buttonHolder").classList.toggle("show");
+			TransitionOptions();
      		activeState = false;
     	}
      	targetedCell = $(this).attr('id');
     });
 
-	$("#link1").on("click", function(){
-		if($(this).text != cellInfo[parseInt($(this).index())][3][0])
-		passedLevel();
+    function shuffle(tbl) {
+        var arr = tbl.find("td");
+        for(
+          var j, x, i = arr.length; i;
+          j = parseInt(Math.random() * i),
+          x = arr[--i], arr[i] = arr[j], arr[j] = x
+        );
+        var tmp;
+        var rows = tbl.find("tr").length
+        var cols = tbl.find("tr:first td").length
+        for (i = 0; i < rows; i++){
+            tmp = tbl.find("tr").eq(i);
+            tmp.html()
+            for (j = 0; j < cols; j++)
+                tmp.append(arr[i*cols+j]);
+        }       
+  }
+
+	$("#link1").on("click", function(){ 
+		clicked = $(this).index(); 
 	})
 	$("#link2").on("click", function(){
-
+		clicked = $(this).index();
 	})
 	$("#link3").on("click", function(){ 
-		if(jQuery.data(info,"test").secInfo.search("training 3") != -1)
-			alert("resisted")
-		else{
-			alert("infected")
-		}
-
+		clicked = $(this).index();
 	})
-     	 
- 	/*
-		$("body").mousemove(function( event ) {
-			if(activeCell == false){
-				$( "#myDropdown").css('left', event.pageX-1+'px');
-			 	$( "#myDropdown").css('top', event.pageY+6+'px');
+	$("#link3").on("click", function(){
+		clicked = $(this).index();
+	})
+
+	$("#buttons td").on("click", function(){
+	   	var arbitraryNumbers = [1, 2, 3, 4];
+		var remove_Item = clicked;
+
+		y = $.grep(arbitraryNumbers, function(value) {
+ 			return value != remove_Item;
+		});
+		if(jQuery.inArray(clicked, arbitraryNumbers) != -1){
+			passedLevel();
+			arbitraryNumbers = [1, 2, 3, 4];
+			$("#"+targetedCell).css("background-color", "red");
+			$("#"+targetedCell).css("pointer-events", "none");
+			TransitionOptions(); 
+			activeState = false;
 		}
-	});
-	*/
+	})
 });
